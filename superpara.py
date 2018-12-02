da7a2863a1d6fc764bf0a30208ddd4aa8378c249
Henry Zhao <zhaohj126@126.com>
2018-12-02 11:41:51 +0800

    learning rate 0.01 when y is 1 results in restart but with y \in [0, 1] no problem !!!

--- superpara.py
+++ superpara.py
@@ -1,8 +1,8 @@
 #working 
 #super parameter for example: dy / dt 
 
-EPOCHS = 50
-LEARN_RATE = - 0.1
+EPOCHS = 100
+LEARN_RATE = - 0.2
 
 DIMENSON = 1
 INPUT_SIZE = DIMENSON + 2
@@ -40,6 +40,9 @@ RAND_SIZE_T = 31
 very strange: when enlarging the initial set the algorithm even performs better?
 RANGE_Y = [0, 1] vs RANGE_Y = [1, 1]
 why!!!!
+
+found the reason: learning rate too small is not good!!! \eita = 0.01; y = 1 (restart) vs \eita = 0.05; y = 1
+but with \eita = 0.01; y \in [0, 1] (no problem) 
 
 how about enlarging T???
 """
